601bb2a590f6f4e7cf49d790922b01fa6a2fec8a
Tyler Cagle <tylerjcagle@gmail.com>
2024-01-09 11:44:08 -0500

    fix mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Column from "@/components/Column";
 
 export default function Home() {
   return (
-    <main className="fixed top-0 left-0 right-0 bottom-0 min-h-screen flex">
+    <main className="fixed top-0 left-0 right-0 bottom-0 h-[100dvh] flex flex-col md:flex-row">
       <Scene />
       <Column />
       {/* <Overlay /> */}
